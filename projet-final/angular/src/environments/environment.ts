e9223f411ba3e42c1f24cf94ba806d988d0af6b0
gourdmandise <rayan.saboun@gmail.com>
2026-03-27 21:08:54 +0100

    mise a jour de environment

--- projet-final/angular/src/environments/environment.ts
+++ projet-final/angular/src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  backendUrl: 'https://api.votredomaine.com',  // ← à remplir avant déploiement
+  backendUrl: 'https://web-commerce-projet.onrender.com',  // ← à remplir avant déploiement
 };
