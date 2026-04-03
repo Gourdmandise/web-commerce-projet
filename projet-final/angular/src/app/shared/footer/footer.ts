1bd8b85e00e6832e1141ba6bc96ff1beba8609b0
gourdmandise <rayan.saboun@gmail.com>
2026-04-03 10:12:13 +0200

    fix(css): ViewEncapsulation.None — remove duplicate imports

--- projet-final/angular/src/app/shared/footer/footer.ts
+++ projet-final/angular/src/app/shared/footer/footer.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 import { RouterLink } from '@angular/router';
 
 @Component({
--- projet-final/angular/src/app/shared/topbar/topbart.ts
+++ projet-final/angular/src/app/shared/topbar/topbart.ts
@@ -1,4 +1,4 @@
-import { Component, ViewEncapsulation, ViewEncapsulation } from '@angular/core';
+import { Component, ViewEncapsulation } from '@angular/core';
 
 @Component({
   selector: 'app-topbar',
