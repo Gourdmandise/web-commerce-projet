613e0360f16b0a6262eacd1d30c7588a4d11b52c
gourdmandise <rayan.saboun@gmail.com>
2026-04-20 17:50:09 +0200

    commune

--- projet-final/angular/src/app/pages/saviez-vous/arret-cuivre/arret-cuivre.ts
+++ projet-final/angular/src/app/pages/saviez-vous/arret-cuivre/arret-cuivre.ts
@@ -16,26 +16,29 @@ export class ArretCuivre {
   private communesService = inject(CommunesService);
 
   communeQuery  = '';
+  suggestions   = signal<Commune[]>([]);
   resultat      = signal<Commune | null>(null);
   chargement    = false;
   erreur        = '';
 
   rechercherCommune() {
     if (this.communeQuery.length < 2) {
+      this.suggestions.set([]);
       this.resultat.set(null);
       return;
     }
 
     this.chargement = true;
     this.erreur = '';
+    this.resultat.set(null);
 
     this.communesService.rechercher(this.communeQuery).subscribe({
       next: (communes) => {
         this.chargement = false;
         if (communes.length > 0) {
-          this.resultat.set(communes[0]);
+          this.suggestions.set(communes);
         } else {
-          this.resultat.set(null);
+          this.suggestions.set([]);
           this.erreur = 'Commune non trouvée. Consultez l\'ARCEP pour plus d\'infos.';
         }
       },
@@ -44,6 +47,17 @@ export class ArretCuivre {
         this.erreur = 'Erreur lors de la recherche.';
       }
     });
+  }
+
+  selectionnerCommune(commune: Commune) {
+    this.communeQuery = `${commune.commune} (${commune.code_postal})`;
+    this.suggestions.set([]);
+    this.resultat.set(commune);
+    this.erreur = '';
+  }
+
+  masquerSuggestions() {
+    setTimeout(() => this.suggestions.set([]), 150);
   }
 
   formatDate(date: string | null): string {
