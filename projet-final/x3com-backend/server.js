7196aee8245de618efe5670e035284f6b6518fb0
gourdmandise <rayan.saboun@gmail.com>
2026-05-07 15:08:25 +0200

    test12

--- projet-final/x3com-backend/server.js
+++ projet-final/x3com-backend/server.js
@@ -1078,7 +1078,7 @@ app.get('/commandes/:id/pdf', requireAuth, async (req, res) => {
       .text('En cas de retard de paiement une pénalité égale à 3 fois le taux d\'intérêt légal sera exigible (Décret 2009-138). Pour les professionnels, une indemnité minimum forfaitaire de 40 euros pour frais de recouvrement sera exigible (Décret 2012-1115).', 40, doc.y, { width: 515 });
 
     doc.moveDown(0.6);
-    doc.fontSize(8).fillColor('#6b7280').text('Siren : 909959843 - APE : 7112B - N°TVA : FR05909959843', 40, doc.y, { width: 515, align: 'center' });
+    doc.fontSize(8).fillColor('#6b7280').text('Siren : 980277933 - APE : 7112B - N°TVA : FR33980277933', 40, doc.y, { width: 515, align: 'center' });
 
     doc.end();
   } catch (err) {
